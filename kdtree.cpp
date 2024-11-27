fece91bbf039f5834581bcce214fb7ac765ea4b9
Hartoe <hartoedepool@gmail.com>
2024-11-27 22:27:02 +0100

    Started adding kD-tree implementation

--- kdtree.cpp
+++ kdtree.cpp
@@ -2,7 +2,7 @@
 
 KDTree::KDTree(Model scene) : scene(scene)
 {
-	// TODO: Construct kD-Tree
+    build_tree();
 }
 
 bool KDTree::scene_intersect(const Vec3f &orig, const Vec3f &dir, Vec3f &hit, Vec3f &N)
@@ -17,4 +17,91 @@ bool KDTree::scene_intersect(const Vec3f &orig, const Vec3f &dir, Vec3f &hit, Ve
         }
     }
     return duck_dist < 1000;
+}
+
+Node KDTree::build_tree()
+{
+    // Gets scene bounding box
+    AABB bounds;
+    scene.get_bbox(bounds.min, bounds.max);
+
+    // Return root node
+    return build_node(bounds, 0, scene.nfaces());
+}
+
+// Recursive call of node building
+Node KDTree::build_node(AABB& bounds, int first, int count, int recursion)
+{
+    // Check if the termination metrics have been met
+    if (terminate_tree(recursion, count))
+    {
+        // Create leaf node with leftover vertices
+        LeafNode node;
+        node.first = first;
+        node.count = count;
+        return node;
+    }
+
+    // Create a plane and split the bounding box
+    Plane plane;
+    AABB right, left;
+    split_along_plane(bounds, left, right, plane);
+    int leftFirst, leftCount, rightFirst, rightCount;
+
+    // Sort faces by bounding boxes
+    get_faces(plane, first, count, leftFirst, leftCount, rightFirst, rightCount);
+
+    // Generate new interior node
+    InteriorNode node;
+    node.plane = plane;
+    node.left = &build_node(left, leftFirst, leftCount, recursion + 1);
+    node.right = &build_node(right, rightFirst, rightCount, recursion + 1);
+    return node;
+}
+
+// Checks if there are less than 10 faces in a bounding box or recursion depth has been reached
+bool terminate_tree(int recursion, int count)
+{
+    return count < 10 || recursion > 20;
+}
+
+// Finds an axis aligned plane within the bounding box
+void split_along_plane(AABB& bounds, AABB& left, AABB& right, Plane& plane)
+{
+    // Get edge lengths
+    Vec3f lengths = bounds.max - bounds.min;
+
+    // Get longest edge
+    int longestEdge = 0;
+    if (lengths[1] > lengths[0] && lengths[1] > lengths[2])
+        longestEdge = 1;
+    else if (lengths[2] > lengths[0] && lengths[2] > lengths[1])
+        longestEdge = 2;
+
+    // Split longest edge and make the two new bounding boxes
+    float offset = lengths[longestEdge] / 2.0f;
+    AABB left, right;
+    left.max = bounds.max;
+    left.min = bounds.min;
+    left.min[longestEdge] += offset;
+    right.min = bounds.min;
+    right.max = bounds.max;
+    right.max[longestEdge] = left.min[longestEdge];
+
+    // Generate the splitting plane of the two new bounding boxes
+    Plane plane;
+    plane.P0 = left.min;
+    plane.P1 = right.max;
+    Vec3f P3 = plane.P0;
+    if (longestEdge == 0)
+        P3[1] = plane.P1[1];
+    else
+        P3[0] = plane.P1[0];
+    plane.N = cross(plane.P0 - plane.P1, plane.P0 - P3).normalize();
+}
+
+// Splits the faces based on a splitting plane
+void get_faces(Plane& plane, int& first, int& count, int& leftFirst, int& leftCount, int& rightFirst, int& rightCount)
+{
+    // TODO: Quick sort the faces of the model around the plane as a pivot
 }
--- kdtree.h
+++ kdtree.h
@@ -2,10 +2,38 @@
 #include "geometry.h"
 #include <vector>
 
+struct Plane
+{
+    Vec3f P0, P1, N; // Start point, mid point, and plane normal
+};
+
+struct Node {};
+
+struct InteriorNode : Node
+{
+    Plane plane;
+    Node* left;
+    Node* right;
+};
+
+struct LeafNode : Node
+{
+    int first; // index to first vertex
+    int count; // amount of vertices in node
+};
+
+struct AABB
+{
+    Vec3f min; // minimum point of the cube
+    Vec3f max; // maximum point of the cube
+};
+
 class KDTree {
 private:
     Model scene;
 public:
     KDTree(Model scene);
     bool scene_intersect(const Vec3f& orig, const Vec3f& dir, Vec3f& hit, Vec3f& N); // Material?
+    Node build_tree();
+    Node build_node(AABB& bounds, int first, int count, int recursion = 1);
 };
